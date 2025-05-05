1e47eea01e7a7d78e1c5521ff3bcb4b28ccb3868
delarea <eyalde@jfrog.com>
2025-05-05 11:19:12 +0300

    Temp - change artifactory default version

--- local-rt-setup/main.go
+++ local-rt-setup/main.go
@@ -29,10 +29,12 @@ const (
 	licenseEnv               = "RTLIC"
 	localArtifactoryUrl      = "http://localhost:8081/artifactory/"
 	// #nosec G101 -- False positive - no hardcoded credentials.
-	tokensApi         = "http://localhost:8082/access/api/v1/tokens"
-	defaultUsername   = "admin"
-	defaultPassword   = "password"
-	defaultVersion    = "[RELEASE]"
+	tokensApi       = "http://localhost:8082/access/api/v1/tokens"
+	defaultUsername = "admin"
+	defaultPassword = "password"
+	// defaultVersion    = "[RELEASE]"
+	// This is a temp patch as the latest release version of artifactory needs extra configuration before it could work.
+	defaultVersion    = "7.104.15"
 	tokenJson         = "token.json"
 	generateTokenJson = "generate.token.json"
 	githubEnvFileEnv  = "GITHUB_ENV"
